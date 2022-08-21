c82916a35e1edf12703db1dfc2c6086104fb3050
Ubuntu <ubuntu@lab-2.maas>
2022-08-21 20:24:54 +0000

    reset to factory

--- old-site/old-site/src/App.js
+++ old-site/old-site/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Testing some <code>React.js</code> code. Check back soon.
         </p>
         <a
           className="App-link"
