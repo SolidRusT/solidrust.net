ce66a139ca8455bb7219ccba0597457b474f693e
Shaun Prince <shaun@techfusion.ca>
2022-08-21 14:04:39 -0700

    update mainpage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SolidRusT Networks UI is getting updated.
         </p>
         <a
           className="App-link"
