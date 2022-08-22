0979ad786239fe37b134c659846878fd03c946ba
Shaun Prince <shaun@techfusion.ca>
2022-08-21 23:53:36 -0700

    reassociate network ip

--- terraform/ec2_instances.tf
+++ terraform/ec2_instances.tf
@@ -1,7 +1,6 @@
 # NGINX public web us-west-2
 resource "aws_instance" "nginx_us_west_2_instance_1" {
   ami                  = data.aws_ami.ubuntu-22-04-arm64-us-west-2.id
-  associate_public_ip_address = true
   instance_type        = local.nginx_instance_type
   availability_zone    = "us-west-2a"
   iam_instance_profile = aws_iam_instance_profile.srt_cloud.name
@@ -9,6 +8,7 @@ resource "aws_instance" "nginx_us_west_2_instance_1" {
   network_interface {
     network_interface_id = aws_network_interface.nginx_1_us_west_2_public.id
     device_index         = 0
+    associate_public_ip_address = true
   }
   network_interface {
     network_interface_id = aws_network_interface.nginx_1_us_west_2_private.id
@@ -30,7 +30,6 @@ resource "aws_instance" "nginx_us_west_2_instance_1" {
 
 resource "aws_instance" "nginx_us_west_2_instance_2" {
   ami                  = data.aws_ami.ubuntu-22-04-arm64-us-west-2.id
-  associate_public_ip_address = true
   instance_type        = local.nginx_instance_type
   availability_zone    = "us-west-2b"
   iam_instance_profile = aws_iam_instance_profile.srt_cloud.name
@@ -38,6 +37,7 @@ resource "aws_instance" "nginx_us_west_2_instance_2" {
   network_interface {
     network_interface_id = aws_network_interface.nginx_2_us_west_2_public.id
     device_index         = 0
+    associate_public_ip_address = true
   }
   network_interface {
     network_interface_id = aws_network_interface.nginx_2_us_west_2_private.id
@@ -60,7 +60,6 @@ resource "aws_instance" "nginx_us_west_2_instance_2" {
 # APP instance us-west-2
 resource "aws_instance" "app_1_us_west_2_private_1" {
   ami                  = data.aws_ami.ubuntu-22-04-x86_64-us-west-2.id
-  associate_public_ip_address = true
   instance_type        = local.app_instance_type
   availability_zone    = "us-west-2a"
   iam_instance_profile = aws_iam_instance_profile.srt_cloud.name
@@ -68,6 +67,7 @@ resource "aws_instance" "app_1_us_west_2_private_1" {
   network_interface {
     network_interface_id = aws_network_interface.app_1_us_west_2_public.id
     device_index         = 0
+    associate_public_ip_address = true
   }
   network_interface {
     network_interface_id = aws_network_interface.app_1_us_west_2_private.id
@@ -90,7 +90,6 @@ resource "aws_instance" "app_1_us_west_2_private_1" {
 # APP instance us-east-1
 resource "aws_instance" "app_1_us_east_1_private_1" {
   ami                  = data.aws_ami.ubuntu-22-04-x86_64-us-east-1.id
-  associate_public_ip_address = true
   instance_type        = local.app_instance_type
   availability_zone    = "us-east-1a"
   iam_instance_profile = aws_iam_instance_profile.srt_cloud.name
@@ -98,6 +97,7 @@ resource "aws_instance" "app_1_us_east_1_private_1" {
   network_interface {
     network_interface_id = aws_network_interface.app_1_us_east_1_public.id
     device_index         = 0
+    associate_public_ip_address = true
   }
   network_interface {
     network_interface_id = aws_network_interface.app_1_us_east_1_private.id
