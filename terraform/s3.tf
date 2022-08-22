c0dc3e5651e819cd9f391124ac105aa013d7b7c0
Shaun Prince <shaun@techfusion.ca>
2022-08-21 23:50:00 -0700

    update providers for s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -21,6 +21,8 @@ resource "aws_s3_bucket" "s3_us_east_1" {
     prevent_destroy = false
   }
 
+  provider = aws.us-east-1
+
   tags = local.common_tags
 }
 
@@ -30,6 +32,7 @@ resource "aws_s3_bucket_public_access_block" "s3_us_east_1" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+  provider = aws.us-east-1
 }
 
 # Default S3 bucket us-east-2
@@ -55,9 +58,9 @@ resource "aws_s3_bucket" "s3_us_east_2" {
     prevent_destroy = false
   }
 
-  tags = local.common_tags
-
   provider = aws.us-east-2
+  
+  tags = local.common_tags
 }
 
 resource "aws_s3_bucket_public_access_block" "s3_us_east_2" {
@@ -68,4 +71,42 @@ resource "aws_s3_bucket_public_access_block" "s3_us_east_2" {
   restrict_public_buckets = true
 
   provider = aws.us-east-2
+}
+
+# Default S3 bucket us-west-2
+
+resource "aws_s3_bucket" "s3_us_west_2" {
+  bucket = "${local.prefix}-s3-us-west-2"
+  acl    = "private"
+  force_destroy = true
+
+  versioning {
+    enabled = false
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  provider = aws.us-west-2
+  
+  tags = local.common_tags
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_us_west_2" {
+  bucket                  = aws_s3_bucket.s3_us_west_2.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  provider = aws.us-west-2
 }
